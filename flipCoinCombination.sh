15f628b6c94c6da5e57bf57ef4bf1cc1ccfac9fe
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-10 14:23:41 +0530

    CHANGED: Doublet combination of coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,30 +1,66 @@
 #! /bin/bash -x
 
-
-echo -e "Display combination of head and tail "
-
 head=1
 tail=0
 count=0
 count1=0
 count2=0
+count3=0
+count4=0
 
-while [ $count != 100 ]
+
+while (( $count != 100 ))
 do
-((count++))
-cointoss=$((RANDOM%2))
-if [[ $cointoss == $head ]]
-then
-        ((count1++))
-        echo "Heads"
-elif [[ $cointoss == $tail ]]
-then
-        ((count2++))
-        echo "Tails"
-fi
+        count_fix=0
+
+                ((count++))
+
+        cointoss=$((RANDOM%2))
+        if(( $cointoss == $head ))
+        then
+                while (( $count_fix != 1 ))
+                do
+                        ((count_fix++))
+                         cointoss=$((RANDOM%2))
+
+
+                        if(( $cointoss == $head ))
+                        then
+
+                                ((count1++))
+
+                        elif(( $cointoss == $tail ))
+                        then
+                                ((count2++))
+                        fi
+                done
+        elif (( $cointoss == $tail ))
+        then
+                while (( $count_fix != 1 ))
+                do
+                        ((count_fix++))
+                         cointoss=$((RANDOM%2))
+
+
+                        if(( $cointoss == $head ))
+                        then
+
+                                ((count3++))
+
+                        elif(( $cointoss == $tail ))
+                        then
+                                ((count4++))
+                        fi
+                done
+        fi
 done
+
 declare -A percentage
-percentage[singlet_Head]=$( echo  $count1 $count | awk '{print $1/$2*100}' )
-percentage[singlet_Tail]=$( echo  $count2 $count | awk '{print $1/$2*100}')
+
+percentage[head_head]=$( echo  $count1 $count | awk '{print $1/$2*100}' )
 
 
+percentage[head_tail]=$( echo $count2 $count | awk '{print $1/$2*100}' )
+percentage[tail_head]=$( echo  $count3 $count | awk '{print $1/$2*100}' )
+
+percentage[tail_tail]=$( echo  $count4 $count | awk '{print $1/$2*100}' )
