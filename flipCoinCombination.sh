2db77b6d0f9b9381f1bc352a4c4823bd8b9f1c23
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-10 14:01:21 +0530

    ADDED: Display Coin Toss

---
+++ flipCoinCombination.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -x
+
+
+echo -e "Please chosse  the \n 1.Head \n 2.Tail "
+
+cointoss1=$((RANDOM%2))
+
+echo "Head  : $cointoss1"
+
+cointoss2=$((RANDOM%2))
+
+echo "Tails : $cointoss2"
+
