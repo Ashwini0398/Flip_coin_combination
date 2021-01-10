5f5523191b30b34b49a257c95082ff617a3365e6
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-10 14:13:47 +0530

    ADDED: singlet combination of coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,11 +3,28 @@
 
 echo -e "Display combination of head and tail "
 
-cointoss1=$((RANDOM%2))
+head=1
+tail=0
+count=0
+count1=0
+count2=0
 
-echo "Head  : $cointoss1"
+while [ $count != 100 ]
+do
+((count++))
+cointoss=$((RANDOM%2))
+if [[ $cointoss == $head ]]
+then
+        ((count1++))
+        echo "Heads"
+elif [[ $cointoss == $tail ]]
+then
+        ((count2++))
+        echo "Tails"
+fi
+done
+declare -A percentage
+percentage[singlet_Head]=$( echo  $count1 $count | awk '{print $1/$2*100}' )
+percentage[singlet_Tail]=$( echo  $count2 $count | awk '{print $1/$2*100}')
 
-cointoss2=$((RANDOM%2))
-
-echo "Tails : $cointoss2"
 
